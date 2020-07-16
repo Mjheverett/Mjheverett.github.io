ca6d9ec566c8209853551291b5610dbb67bf1690
Matthew Everett <mjheverett@gmail.com>
2020-07-16 17:42:30 -0400

    update

--- scripts/main.js
+++ scripts/main.js
@@ -10,29 +10,3 @@ myImage.onclick = function() {
       myImage.setAttribute ('src','images/profile.jpg');
     }
 }
-
-//Personalize welcome message code
-
-let myButton = document.querySelector('button');
-let myHeading = document.querySelector('h1');
-
-function setUserName() {
-    let myName = prompt('Please enter your name.');
-    if(!myName) {
-      setUserName();
-    } else {
-      localStorage.setItem('name', myName);
-      myHeading.innerHTML = 'Mozilla is cool, ' + myName;
-    }
-}
-
-if(!localStorage.getItem('name')) {
-    setUserName();
-  } else {
-    let storedName = localStorage.getItem('name');
-    myHeading.textContent = 'Mozilla is cool, ' + storedName;
-}  
-
-myButton.onclick = function() {
-    setUserName();
-}  
